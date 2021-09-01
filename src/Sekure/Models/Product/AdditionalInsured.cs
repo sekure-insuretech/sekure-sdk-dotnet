4cfb9a2a560f9a2e42f13a7a677ede57fa7a61e7
Alejandra Sandoval <developer1@sekure.com.co>
2021-08-31 21:50:29 -0500

    feat: se agregan las siguientes propiedades el valor asegurado y lista de asegurados adicionales.

---
+++ src/Sekure/Models/Product/AdditionalInsured.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Sekure.Models
+{
+    public class AdditionalInsured
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Value { get; set; }
+        public List<CoverageResultAPI> Coverages { get; set; }
+
+        public AdditionalInsured(string name, string description, string value, List<CoverageResultAPI> coverages)
+        {
+            Name = name;
+            Description = description;
+            Value = value;
+            Coverages = coverages;
+        }
+    }
+}
--- src/Sekure/Models/Product/Quote.cs
+++ src/Sekure/Models/Product/Quote.cs
@@ -5,6 +5,7 @@ namespace Sekure.Models
     public class Quote
     {
         public string PlanId { get; set; }
+        public string InsuredValue { get; set; }
         public List<CoverageResultAPI> Coverages { get; set; }
         public List<InfoResultAPI> Deductible { get; set; }
         public string StartDate { get; set; }
@@ -14,11 +15,14 @@ namespace Sekure.Models
         public List<InfoResultAPI> Beneficiaries { get; set; }
         public List<InfoResultAPI> GracePeriodsList { get; set; }
         public List<InputParameter> AdditionalInfo { get; set; }
+        public List<AdditionalInsured> AdditionalInsured { get; set; }
 
         public Quote() { }
 
-        public Quote(List<CoverageResultAPI> coverages, List<InfoResultAPI> deductible, string startDate, string termTime, string premiumAmount, string premiumPaymentInterval, List<InfoResultAPI> beneficiaries, List<InfoResultAPI> gracePeriodsList, List<InputParameter> additionalInfo)
+        public Quote(string planId, string insuredValue, List<CoverageResultAPI> coverages, List<InfoResultAPI> deductible, string startDate, string termTime, string premiumAmount, string premiumPaymentInterval, List<InfoResultAPI> beneficiaries, List<InfoResultAPI> gracePeriodsList, List<InputParameter> additionalInfo, List<AdditionalInsured> additionalInsured)
         {
+            PlanId = planId;
+            InsuredValue = insuredValue;
             Coverages = coverages;
             Deductible = deductible;
             StartDate = startDate;
@@ -28,6 +32,7 @@ namespace Sekure.Models
             Beneficiaries = beneficiaries;
             GracePeriodsList = gracePeriodsList;
             AdditionalInfo = additionalInfo;
+            AdditionalInsured = additionalInsured;
         }
     }
 
