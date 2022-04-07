589e8ff2975c4449590df5e69398d13be6ed2349
deybisuanca07 <deibymfc18@gmail.com>
2022-04-07 13:59:56 -0500

    add news models

---
+++ src/Sekure/Models/TenantContact/TenantContact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sekure.Models
+{
+    public class TenantContact
+    {
+        public int Id { get; set; }
+        public Guid TenantId { get; set; }
+        public virtual Tenant Tenant { get; set; }
+        public string Email { get; set; }
+        public string Details { get; set; }
+        public virtual List<Session> Sessions { get; set; }
+        public virtual List<Estimate> Estimates { get; set; }
+        public virtual List<Product> Products { get; set; }
+        public virtual List<Payment> Payments { get; set; }
+
+        public TenantContact(int id, Guid tenantId, string email, string details)
+        {
+            Id = id;
+            TenantId = tenantId;
+            Email = email;
+            Details = details;
+        }
+    }
+}
