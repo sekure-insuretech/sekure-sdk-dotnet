e655b135dc6c8701b8d1ab1efa9f848f3b6fb345
JhotaMS <Jhotams.developer@gmail.com>
2022-06-16 01:53:29 -0500

    Se agrega las propiedades de ProductCover y DevoleperGuide al modelo ProductReference

--- src/Sekure/Models/Product/ProductReference.cs
+++ src/Sekure/Models/Product/ProductReference.cs
@@ -6,14 +6,19 @@
         public string Name { get; set; }
         public string PolicyTypeName { get; set; }
         public string InsuranceCompanyName { get; set; }
+        public string ProductCover { get; set; }
+        public string DevoleperGuide { get; set; }
 
         public ProductReference() { }
-        public ProductReference(int id, string name, string policyTypeName, string insuranceCompanyName)
+
+        public ProductReference(int id, string name, string policyTypeName, string insuranceCompanyName, string productCover, string devoleperGuide)
         {
             Id = id;
             Name = name;
             PolicyTypeName = policyTypeName;
             InsuranceCompanyName = insuranceCompanyName;
+            ProductCover = productCover;
+            DevoleperGuide = devoleperGuide;
         }
     }
 }
