8ad39dd9942c8535a7460268d46f7652356b4877
JhotaMS <Jhotams.developer@gmail.com>
2022-08-04 01:10:36 -0500

    Se le agrega parametros al policyHolder

--- src/Sekure/Models/Product/PolicyHolder.cs
+++ src/Sekure/Models/Product/PolicyHolder.cs
@@ -8,7 +8,8 @@ namespace Sekure.Models
         public string SecondName { get; set; }
         public string LastName { get; set; }
         public string SecondLastName { get; set; }
-        public DateTime Birthdate { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public DateTime? ExpeditionDate { get; set; }
         public string Gender { get; set; }
         public string Address { get; set; }
         public string IdentificationType { get; set; }
@@ -16,16 +17,18 @@ namespace Sekure.Models
         public string MaritalStatus { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        public string CityCode { get; set; }
 
         public PolicyHolder() { }
 
-        public PolicyHolder(string firstName, string secondName, string lastName, string secondLastName, DateTime birthdate, string gender, string address, string identificationType, string identificationNumber, string maritalStatus, string email, string phoneNumber)
+        public PolicyHolder(string firstName, string secondName, string lastName, string secondLastName, DateTime? birthdate, DateTime? expeditionDate, string gender, string address, string identificationType, string identificationNumber, string maritalStatus, string email, string phoneNumber, string cityCode)
         {
             FirstName = firstName;
             SecondName = secondName;
             LastName = lastName;
             SecondLastName = secondLastName;
             Birthdate = birthdate;
+            ExpeditionDate = expeditionDate;
             Gender = gender;
             Address = address;
             IdentificationType = identificationType;
@@ -33,6 +36,7 @@ namespace Sekure.Models
             MaritalStatus = maritalStatus;
             Email = email;
             PhoneNumber = phoneNumber;
+            CityCode = cityCode;
         }
     }
 }
