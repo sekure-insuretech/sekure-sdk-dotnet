7f9947bd5f801df152f0cdb70b18bf3683d77a4d
JhotaMS <Jhotams.developer@gmail.com>
2022-08-19 11:26:18 -0500

    Nuevas propiedades en el modelo de PolicyHolder

--- src/Sekure/Models/Product/PolicyHolder.cs
+++ src/Sekure/Models/Product/PolicyHolder.cs
@@ -8,35 +8,54 @@ namespace Sekure.Models
         public string SecondName { get; set; }
         public string LastName { get; set; }
         public string SecondLastName { get; set; }
-        public DateTime? Birthdate { get; set; }
-        public DateTime? ExpeditionDate { get; set; }
         public string Gender { get; set; }
         public string Address { get; set; }
         public string IdentificationType { get; set; }
         public string IdentificationNumber { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public DateTime? ExpeditionDate { get; set; }
         public string MaritalStatus { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string CityCode { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyIdentificationNumber { get; set; }
+        public string CompanyEmail { get; set; }
+        public string CompanyPhone { get; set; }
+        public DateTime? CompanyDate { get; set; }
+        public string CompanyPostalCode { get; set; }
+        public string CompanyStreetNumber { get; set; }
+        public string CompanyAddress { get; set; }
 
-        public PolicyHolder() { }
-
-        public PolicyHolder(string firstName, string secondName, string lastName, string secondLastName, DateTime? birthdate, DateTime? expeditionDate, string gender, string address, string identificationType, string identificationNumber, string maritalStatus, string email, string phoneNumber, string cityCode)
+        public PolicyHolder(string firstName, string secondName, string lastName, string secondLastName, string gender, string address, string identificationType, string identificationNumber, DateTime? birthdate, DateTime? expeditionDate, string maritalStatus, string email, string phoneNumber, string cityCode, string companyName, string companyIdentificationNumber, string companyEmail, string companyPhone, DateTime? companyDate, string companyPostalCode, string companyStreetNumber, string companyAddress)
         {
             FirstName = firstName;
             SecondName = secondName;
             LastName = lastName;
             SecondLastName = secondLastName;
-            Birthdate = birthdate;
-            ExpeditionDate = expeditionDate;
             Gender = gender;
             Address = address;
             IdentificationType = identificationType;
             IdentificationNumber = identificationNumber;
+            Birthdate = birthdate;
+            ExpeditionDate = expeditionDate;
             MaritalStatus = maritalStatus;
             Email = email;
             PhoneNumber = phoneNumber;
             CityCode = cityCode;
+            CompanyName = companyName;
+            CompanyIdentificationNumber = companyIdentificationNumber;
+            CompanyEmail = companyEmail;
+            CompanyPhone = companyPhone;
+            CompanyDate = companyDate;
+            CompanyPostalCode = companyPostalCode;
+            CompanyStreetNumber = companyStreetNumber;
+            CompanyAddress = companyAddress;
+        }
+
+        public PolicyHolder()
+        {
+
         }
     }
 }
