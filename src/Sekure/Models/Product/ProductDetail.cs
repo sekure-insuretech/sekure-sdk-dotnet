573ffc1853948a67fdadcf5c438759f24329eb1d
JhotaMS <Jhotams.developer@gmail.com>
2022-09-20 13:26:01 -0500

    Corrección en el modelo de QuotedProduct

--- src/Sekure/Models/Product/ProductDetail.cs
+++ src/Sekure/Models/Product/ProductDetail.cs
@@ -6,15 +6,19 @@
         public string ProductName { get; set; }
         public string PolicyTypeName { get; set; }
         public string InsuranceCompanyName { get; set; }
+        public string ProductCover { get; set; }
+        public string DeveloperGuide { get; set; }
 
         public ProductDetail() { }
 
-        public ProductDetail(int productId, string productName, string policyTypeName, string insuranceCompanyName)
+        public ProductDetail(int productId, string productName, string policyTypeName, string insuranceCompanyName, string productCover, string developerGuide)
         {
             ProductId = productId;
             ProductName = productName;
             PolicyTypeName = policyTypeName;
             InsuranceCompanyName = insuranceCompanyName;
+            ProductCover = productCover;
+            DeveloperGuide = developerGuide;
         }
     }
 }
--- src/Sekure/Models/Product/Quote.cs
+++ src/Sekure/Models/Product/Quote.cs
@@ -6,70 +6,57 @@ namespace Sekure.Models
     public class Quote
     {
         public Guid SessionId { get; set; }
-        public string IdPlan { get; set; }
+        public string PlanId { get; set; }
         public string PlanNumber { get; set; }
         public string PlanName { get; set; }
-        public string QuotationValueMonthly { get; set; }
-        public string QuotationValueAnnual { get; set; }
-        public string ProductName { get; set; }
-        public string InsuranceCompanyName { get; set; }
-        public string CoverageStartDate { get; set; }
-        public string CoverageEndDate { get; set; }
-        public string ExpeditionDate { get; set; }
-        public string QuoteMessage { get; set; }
-        public string AmountInsured { get; set; }
-        public string QuotationTotal { get; set; }
+        public string PremiumAmount { get; set; }
+        public string PremiumPaymentInterval { get; set; }
+        public string StartDate { get; set; }
+        public string TermTime { get; set; }
+        public string InsuredValue { get; set; }
+
         public string PolicyNumber { get; set; }
         public string EmailSubject { get; set; }
         public string EmailBody { get; set; }
         public string EmailPolicy { get; set; }
         public string AttachName { get; set; }
-
-        public List<CoverageResultApi> CoverageResultList { get; set; }
-        public List<CoverageResultApi> AggregatesResultList { get; set; }
-        public List<InfoResultApi> InsuredResultList { get; set; }
-        public List<InfoResultApi> GracePeriodsList { get; set; }
-        public List<InfoResultApi> DeductibleList { get; set; }
-        public List<InputParameter> OtherInformationOS { get; set; }
-        public List<AdditionalInsured> AdditionalInsured { get; set; }
-        public EndOfValidityPremium EndOfValidityPremium { get; set; }
         public List<AdditionalInfo> QuoteInfo { get; set; }
+        public List<CoverageResultApi> Coverages { get; set; }
+        public List<InfoResultApi> Deductible { get; set; }
+        public string ExpeditionDate { get; set; }
+        public string QuoteMessage { get; set; }
+        public List<InfoResultApi> Beneficiaries { get; set; }
+        public List<InfoResultApi> GracePeriodsList { get; set; }
+        public List<InputParameter> AdditionalInfo { get; set; }
+        public List<AdditionalInsured> AdditionalInsured { get; set; }
 
-        public Quote()
-        {
+        public Quote() { }
 
-        }
-
-        public Quote(Guid sessionId, string idPlan, string planNumber, string planName, string quotationValueMonthly, string quotationValueAnnual, string productName, string insuranceCompanyName, string coverageStartDate, string coverageEndDate, string expeditionDate, string quoteMessage, string amountInsured, string quotationTotal, string policyNumber, string emailSubject, string emailBody, string emailPolicy, string attachName, List<CoverageResultApi> coverageResultList, List<CoverageResultApi> aggregatesResultList, List<InfoResultApi> insuredResultList, List<InfoResultApi> gracePeriodsList, List<InfoResultApi> deductibleList, List<InputParameter> otherInformationOS, List<AdditionalInsured> additionalInsured, EndOfValidityPremium endOfValidityPremium, List<AdditionalInfo> quoteInfo)
+        public Quote(Guid sessionId, string planId, string planNumber, string planName, string premiumAmount, string premiumPaymentInterval, string startDate, string termTime, string insuredValue, string policyNumber, string emailSubject, string emailBody, string emailPolicy, string attachName, List<AdditionalInfo> quoteInfo, List<CoverageResultApi> coverages, List<InfoResultApi> deductible, string expeditionDate, string quoteMessage, List<InfoResultApi> beneficiaries, List<InfoResultApi> gracePeriodsList, List<InputParameter> additionalInfo, List<AdditionalInsured> additionalInsured)
         {
             SessionId = sessionId;
-            IdPlan = idPlan;
+            PlanId = planId;
             PlanNumber = planNumber;
             PlanName = planName;
-            QuotationValueMonthly = quotationValueMonthly;
-            QuotationValueAnnual = quotationValueAnnual;
-            ProductName = productName;
-            InsuranceCompanyName = insuranceCompanyName;
-            CoverageStartDate = coverageStartDate;
-            CoverageEndDate = coverageEndDate;
-            ExpeditionDate = expeditionDate;
-            QuoteMessage = quoteMessage;
-            AmountInsured = amountInsured;
-            QuotationTotal = quotationTotal;
+            PremiumAmount = premiumAmount;
+            PremiumPaymentInterval = premiumPaymentInterval;
+            StartDate = startDate;
+            TermTime = termTime;
+            InsuredValue = insuredValue;
             PolicyNumber = policyNumber;
             EmailSubject = emailSubject;
             EmailBody = emailBody;
             EmailPolicy = emailPolicy;
             AttachName = attachName;
-            CoverageResultList = coverageResultList;
-            AggregatesResultList = aggregatesResultList;
-            InsuredResultList = insuredResultList;
-            GracePeriodsList = gracePeriodsList;
-            DeductibleList = deductibleList;
-            OtherInformationOS = otherInformationOS;
-            AdditionalInsured = additionalInsured;
-            EndOfValidityPremium = endOfValidityPremium;
             QuoteInfo = quoteInfo;
+            Coverages = coverages;
+            Deductible = deductible;
+            ExpeditionDate = expeditionDate;
+            QuoteMessage = quoteMessage;
+            Beneficiaries = beneficiaries;
+            GracePeriodsList = gracePeriodsList;
+            AdditionalInfo = additionalInfo;
+            AdditionalInsured = additionalInsured;
         }
     }
 
